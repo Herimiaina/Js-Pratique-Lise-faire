c61d9fc0c0d28bf19441ca05a6e734511131c410
Herimiaina <herimiaina.ras@gmail.com>
2016-10-23 23:33:12 +0200

    Version 12

--- main.js
+++ main.js
@@ -1,113 +1,142 @@
-var toDoList = {
-  todos: [],
-  // - displayToDos devrait montrer le text
-  // - toDoList.addToDo ajoute aussi des objets
-  addToDo: function(newValue) {
-    this.todos.push({
-      text: newValue,
-      completee: false,
-    });
-  },
-  // - toDoList.editToDo change la propriété texte de l'objet todo
-  editToDo: function(index, newValue){
-    var i = index - 1;
-    this.todos[i].text = newValue;
-  },
-  // - une méthode pour supprimer un "à faire"
-  rmToDo: function (index) {
-    var i = index - 1;
-    this.todos.splice(i, 1);
-  },
-  // - toDoList.toggleCompletee change la propriété completee
-  toogleCompletee: function(index) {
-    var i = index - 1;
-    var todo = this.todos[i];
-    todo.completee = !todo.completee;
-  },
-  toogleAll: function () {
-    var numbrCompletee = 0;
-    for (var i = 0 ; i < this.todos.length ; i++) {
-      if (this.todos[i].completee === true) {
-        numbrCompletee++;
-      }
-    }     
-//V6
-    if (numbrCompletee === this.todos.length) {
-      // - si tout est checkée, tout déchecké
-      for( i = 0 ; i < this.todos.length; i++) {
-        this.todos[i].completee = false;
-      }
-    // - si tout est déchecké, tout checkée
-    } else {
-      for ( i = 0 ; i < this.todos.length ; i++) {
-        this.todos[i].completee = true;
-      }
-    }
-  },
+var uneListe = {
+	liste: [],
+	afficher: function () {
+		console.log("Contenu de la liste");
+		if (this.liste.length === 0) {
+			console.log("Vide");
+		} else {
+			this.liste.forEach(function(entree){
+				if(entree.check === false){
+					console.log("( ) " + entree.text);
+				} else {
+					console.log("(X) " + entree.text);
+				}
+			});	
+		}
+	},
+	ajouter: function (entree) {
+		this.liste.push({
+			text: entree,
+			check: false,
+		});
+		console.log("Ajout " + entree);
+	},
+	modifier: function (ordre, entree) {
+		var index = ordre - 1;
+		this.liste[index].text = entree;
+		if (this.liste[index].check === true) {
+			this.checking(ordre);
+		}
+		console.log("Modif numero " + ordre + " : " + entree);
+	},
+	supprimer: function (ordre) {
+		var index = ordre - 1;
+		console.log("Suppression " + ordre + " : " + this.liste[index]);
+		this.liste.splice(index, 1);
+	},
+
+	checking: function (ordre) {
+		var index = ordre - 1;
+		this.liste[index].check = !this.liste[index].check;
+	},
+	toggleAll: function () {
+		var nbrCheck = 0;
+		this.liste.forEach(function(entree){
+			if (entree.check === true) {
+				nbrCheck++;
+			}
+		});
+		this.liste.forEach(function(entree){
+			if (nbrCheck === this.liste.length) {
+				entree.check = false; 
+			} else {
+				entree.check = true;
+			}
+		}, this);	
+	},
 }
 
-//V7
-
-//- idya un boutton displayToDos() et un boutton toogleAll()
 
 var handlers = {
-// //- Cliquer sur le boutton "liste à faire" devrait lancer toDoList.displayToDos();
-// //- Cliquer sur le boutton "toogle" devrait lancer toDoList.toogleAll();
-  toogleAll: function(){
-    toDoList.toogleAll();
-    view.displayToDos();
-  },  
-// V8
-// ida des controles pour .addToDo
-  addToDo: function(){
-    var addTxtInP = document.getElementById("addTxtInP");
-    toDoList.addToDo(addTxtInP.value);
-    addTxtInP.value = "";
-    view.displayToDos();
-  },
-// ida des controles pour .editToDo
-  editToDo: function(){
-    var position = document.getElementById("positionToEdit");
-    var newValue = document.getElementById("newValue").value;
-    toDoList.editToDo(position.valueAsNumber, newValue);
-    position.value = "";
-    newValue = "";
-    view.displayToDos();
-  },
-// ida des controles pour .rmToDo
-  rmToDo: function(){
-    var position = document.getElementById("positionToDelete");
-    toDoList.rmToDo(position.valueAsNumber);
-    position.value = "";
-    view.displayToDos();
-  },
-// ida des controles pour .toogleCompletee
-  toogleToDo: function() {
-    var position = document.getElementById("positionToToogle");
-    toDoList.toogleCompletee(position.valueAsNumber);
-    position.value = "";
-    view.displayToDos();
-  },
+	toggleAll: function(){
+		uneListe.toggleAll();
+		view.afficher();
+	},
+	ajouter: function(){
+		var ajout = document.getElementById("ajout");
+		uneListe.ajouter(ajout.value);
+		ajout.value = "";
+		view.afficher();
+	},
+	modifier: function(){
+		var modif = document.getElementById("modif");
+		var position = document.getElementById("modifPosition");
+		uneListe.modifier(position.valueAsNumber, modif.value);
+		modif.value = "";
+		position.value = "";
+		view.afficher();
+	},
+	supprimer: function(ordre){
+		uneListe.supprimer(ordre);
+		view.afficher();
+	},
+	checking: function(ordre){
+		uneListe.checking(ordre);
+		view.afficher();
+	},
+	setEventListen: function(){
+		var listeOl = document.querySelector("ol");
+		listeOl.addEventListener("click", function(event){
+			var clickedBtn = event.target;
+			var ordre = parseInt(clickedBtn.parentNode.id);
+			if (clickedBtn.className === "check") {
+				this.checking(ordre);
+			} else if (clickedBtn.className === "delete") {
+				this.supprimer(ordre);
+			}
+		});
+	}
 }
+handlers.setEventListen();
+
 
 var view = {
-  displayToDos: function(){
-    var todosOl = document.querySelector("ol");
-    todosOl.innerHTML = "";
-    for (var i = 0; i < toDoList.todos.length ; i++) {
-        var todoLi = document.createElement("li");
-        var todo = toDoList.todos[i];
-        var todoTextComp = "";
+	afficher: function(){
+		var listeOl = document.querySelector("ol");
+		listeOl.innerHTML = "";
 
-        if (todo.completee === true) {
-          todoTextComp = "(X) " + todo.text;
-        } else {
-          todoTextComp = "( ) " + todo.text;
-        }
-        todoLi.textContent = todoTextComp;
-        todosOl.appendChild(todoLi);
-    }
-  },
-};
+		if (uneListe.liste.length === 0) {
+			listeOl.innerHTML = "vide";
+		}
 
-  
+		uneListe.liste.forEach(function(entree, index){
+			var listeIl = document.createElement("li");
+			var textp = document.createElement("p");
+			var textComposition = "";
+
+			if (entree.check === false) {
+				textComposition = "( ) " + entree.text;
+			} else {
+				textComposition = "(X) " + entree.text;
+			}
+			listeIl.id = index + 1;
+			textp.textContent = textComposition;
+			listeIl.appendChild(textp);
+			listeIl.appendChild(this.createBtnCheck());
+			listeIl.appendChild(this.createBtnDel());
+			listeOl.appendChild(listeIl);
+		}, this);
+	},
+	createBtnCheck: function(){
+		var btnCheck = document.createElement("button");
+		btnCheck.textContent = "Check";
+		btnCheck.className = "check";
+		return btnCheck;
+	},
+	createBtnDel: function(){
+		var btnDel = document.createElement("button");
+		btnDel.textContent = "Supprimer";
+		btnDel.className = "delete";
+		return btnDel;
+	}
+}
